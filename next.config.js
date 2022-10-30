b53bf7d274ba1a6abd3221dd4b4eeeb068cb6293
Micheaaa <91579887+micheaaa@users.noreply.github.com>
2022-10-30 16:58:09 -0500

    Update tsconfig and next config to support orm

--- next.config.js
+++ next.config.js
@@ -1,7 +1,34 @@
-/** @type {import('next').NextConfig} */
+const typeCompiler = require('@deepkit/type-compiler')
+
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.ts$/,
+      use: {
+        loader: 'ts-loader',
+        options: {
+          compilerOptions: {
+            noEmit: false
+          },
+          getCustomTransformers: () => ({
+            before: [typeCompiler.transformer],
+            afterDeclarations: [typeCompiler.declarationTransformer],
+          }),
+        }
+      },
+      exclude: /node_modules/,
+    })
+
+    return config
+  },
+  experimental: {
+    appDir: true,
+    enableUndici: true,
+  },
 }
 
 module.exports = nextConfig
